5ccd0bf198f375982286d92696d7504e43385705
satindersatty <satinder.satty@gmail.com>
2016-12-17 16:39:09 -0500

    * .gitignore: First draft of the of the ContentPage code
    * Main.cs:
    * Info.plist:
    * AppDelegate.cs:
    * packages.config:
    * packages.config:
    * MainActivity.cs:
    * Entitlements.plist:
    * ContentPageLearning.sln:
    * LaunchScreen.storyboard:
    * AboutAssets.txt:
    * App.xaml:
    * Page1.xaml:
    * App.xaml.cs:
    * AssemblyInfo.cs:
    * styles.xml:
    * Contents.json:
    * Page1.xaml.cs:
    * icon.png:
    * ContentPageLearning.iOS.csproj:
    * Tabbar.axml:
    * AboutResources.txt:
    * Toolbar.axml:
    * Resource.designer.cs:
    * AndroidManifest.xml:
    * ContentPageLearning.Droid.csproj:
    * icon.png:
    * icon.png:
    * icon.png:
    * ContentPageLearning.shproj:
    * ContentPageLearningPage.xaml:
    * ContentPageLearning.projitems:
    * ContentPageLearningPage.xaml.cs:
    * Contents.json:

---
+++ ContentPageLearning/ContentPageLearningPage.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Xamarin.Forms;
+
+namespace ContentPageLearning
+{
+	public partial class ContentPageLearningPage : ContentPage
+	{
+		public ContentPageLearningPage()
+		{
+			InitializeComponent();
+		}
+	}
+}
